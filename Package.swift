75e2b2104f08cf7eeb87b555856aa30be934dfeb
Devansh Seth <dev.devanshseth.engg0072@gmail.com>
2025-03-03 04:42:28 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GridKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "GridKit",
+            targets: ["GridKit"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GridKit",
+            url: "https://github.com/Devansh-Seth-DEV/GridKit/releases/download/v1.0.0/GridKit.xcframework.zip",
+            checksum: "64f996064e9041ddb87ac6c5396db6a2ce158c0aa38aab0643459c362e2e633b"
+        ),
+    ]
+)
