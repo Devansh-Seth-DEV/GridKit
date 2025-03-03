89138cba32e1146457e184ab5d60171b0f2d02dc
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2025-03-03 05:59:23 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@
 
 import PackageDescription
 
-var majorVersion: String { "1.0.0" }
-
-var minorVersion: String { "1.0.0" }
-
 let package = Package(
     name: "GridKit",
     platforms: [
@@ -21,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GridKit",
-            url: "https://github.com/Devansh-Seth-DEV/GridKit/releases/download/v\(majorVersion)/GridKit.xcframework.zip",
+            url: "https://github.com/Devansh-Seth-DEV/GridKit/releases/download/v1.0.0/GridKit.xcframework.zip",
             checksum: "64f996064e9041ddb87ac6c5396db6a2ce158c0aa38aab0643459c362e2e633b"
         ),
     ]
